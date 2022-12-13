99fa15037edeef54259d28044f16b8b7913edca4
Drew Chase <dcmanproductions@gmail.com>
2022-12-13 01:41:10 -0500

    Added constants for application directories

---
+++ Quasarr.Core/Locations.cs
@@ -0,0 +1,9 @@
+﻿namespace Quasarr.Core;
+
+public static class Locations
+{
+    public static string Root => Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ApplicationData.Company, ApplicationData.ApplicationName)).FullName;
+    public static string Data => Directory.CreateDirectory(Path.Combine(Root, "data")).FullName;
+    public static string Logs => Directory.CreateDirectory(Path.Combine(Root, "logs")).FullName;
+    public static string Temp => Directory.CreateDirectory(Path.Combine(Root, "tmp")).FullName;
+}
