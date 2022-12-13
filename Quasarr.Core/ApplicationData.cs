ad764fe9a5854e585065e8f0be4d4d935eae0c35
Drew Chase <dcmanproductions@gmail.com>
2022-12-13 01:40:48 -0500

    Added constants for Application specific information

---
+++ Quasarr.Core/ApplicationData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quasarr.Core;
+
+public static class ApplicationData
+{
+    public static string ApplicationName => "Quasarr";
+    public static string Company => "LFInteractive";
+    
+}
