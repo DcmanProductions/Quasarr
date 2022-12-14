9baa9c23e6d981f78891d086f84a495094f126b6
Drew Chase <dcmanproductions@gmail.com>
2022-12-13 23:51:30 -0500

    Added task duration

--- Quasarr.Tester/Program.cs
+++ Quasarr.Tester/Program.cs
@@ -7,9 +7,12 @@ internal class Program
 {
     private static void Main()
     {
+        long start = DateTime.Now.Ticks;
         Console.WriteLine("Initializing Tests...");
-        //new TestSABnzbd().Start().Wait();
-        //new TestNetworking().Start().Wait();
+        new TestSABnzbd().Start().Wait();
+        new TestNetworking().Start().Wait();
         new TestSonarr().Start().Wait();
+        TimeSpan after = new(DateTime.Now.Ticks - start);
+        Console.WriteLine($"\n\n\nTest took {after.Minutes}m {after.Seconds}s {after.Milliseconds}ms");
     }
 }
