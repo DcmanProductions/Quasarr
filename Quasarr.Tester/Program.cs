0c3fa5cfdf093f6006d2f2f987891365044f955f
Drew Chase <dcmanproductions@gmail.com>
2022-12-13 23:13:25 -0500

    Added test for sonarr

--- Quasarr.Tester/Program.cs
+++ Quasarr.Tester/Program.cs
@@ -8,7 +8,8 @@ internal class Program
     private static void Main()
     {
         Console.WriteLine("Initializing Tests...");
-        new TestSABnzbd().Start().Wait();
-        new TestNetworking().Start().Wait();
+        //new TestSABnzbd().Start().Wait();
+        //new TestNetworking().Start().Wait();
+        new TestSonarr().Start().Wait();
     }
 }
---
+++ Quasarr.Tester/Tests/TestSonarr.cs
@@ -0,0 +1,30 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+using Quasarr.Sonarr.Collections;
+
+namespace Quasarr.Tester.Tests
+{
+    internal class TestSonarr : TestBase
+    {
+        public TestSonarr() : base("Sonarr",
+            new()
+            {
+            {
+                "Poll",
+                new(() =>
+                {
+                    try
+                    {
+                        SeriesCollection series = SeriesCollection.Poll();
+                        return true;
+                    }
+                    catch(Exception ex)
+                    {
+                        return false;
+                    }
+                })
+            }
+            })
+        {
+        }
+    }
+}
